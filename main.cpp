62c9d4264d12dffb1720deb8e1a7191570df3d32
Benjay2K <benilepuschitz@gmail.com>
2022-03-22 20:26:13 +0100

    Ex1.3 (sum of two integers)

--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@ int main() {
     printf("Hello_University_of_Klagenfurt!\n");
     getchar();
 
-    
+    int number1, number2, sum;
+    printf("Please enter two integers:\n");
+    scanf("%d %d",&number1,&number2);
+
+    sum = number1 + number2;
+
+    printf("%d + %d = %d",number1, number2, sum);
 
     return 0;
 
