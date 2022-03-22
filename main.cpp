27fdf441341e20034c3ef1832b1d01a8406815a3
Benjay2K <benilepuschitz@gmail.com>
2022-03-22 20:23:40 +0100

    init + Ex1.2 (printf: "Hello_University_of_Klagenfurt!\n")

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#define CRT SECURE NO WARNINGS
+#include<stdio.h>
+#include <iostream>
+
+int main() {
+
+    printf("Hello_University_of_Klagenfurt!\n");
+    getchar();
+
+    
+
+    return 0;
+
+
+}
